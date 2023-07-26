f4e4733ae764efb831476f0172b8539b5da56815
EmilSundman <sundman.emil@gmail.com>
2023-07-26 20:57:31 +0200

    first assets from kaggle thingys

--- location_data_generators/assets/data_generators/data_generators.py
+++
@@ -1,9 +0,0 @@
-from dagster import asset, AssetIn, SourceAsset, AssetKey
-
-
-source_asset = SourceAsset(key=AssetKey("some_asset"))
-
-@asset()
-def cool_asset(some_asset):
-    upstream = some_asset
-    return "Hello Mom"
