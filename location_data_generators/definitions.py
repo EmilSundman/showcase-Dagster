e8a1bd3f4eee521491a3d9623f84ca5f7aad79f6
EmilSundman <sundman.emil@gmail.com>
2023-07-27 21:26:19 +0200

    aaa

--- location_data_generators/definitions.py
+++ location_data_generators/definitions.py
@@ -1,17 +1,29 @@
+import logging
+import json
+from pathlib import Path
 import os
 
 from dagster import (
     Definitions,
     load_assets_from_package_module,
-    file_relative_path
+    file_relative_path, 
+    OpExecutionContext
 )
+
 from dagster_duckdb_pandas import DuckDBPandasIOManager
+from dagster_dbt.asset_decorator import dbt_assets
+from dagster_dbt import dbt_cli_resource, load_assets_from_dbt_project, load_assets_from_dbt_manifest
+
 from location_data_generators.assets import raw_data
-from dagster_dbt import dbt_cli_resource, load_assets_from_dbt_project
 
 
-DBT_PROJECT_DIR = file_relative_path(__file__, "dbt_transformations")
-DBT_PROFILES_DIR = file_relative_path(__file__, "dbt_transformations/config")
+MANIFEST_PATH = file_relative_path(__file__, "../../dbt_transformations/target/manifest.json")
+DBT_PROJECT_DIR = file_relative_path(__file__, "../../dbt_transformations")
+DBT_PROFILES_DIR = file_relative_path(__file__, "../../dbt_transformations/config")
+
+MANIFEST_PATH = "./dbt_transformations/target/manifest.json"
+DBT_PROJECT_DIR = "./dbt_transformations"
+DBT_PROFILES_DIR = "./dbt_transformations/config"
 
 raw_data_assets = load_assets_from_package_module(
     raw_data,
@@ -20,12 +32,17 @@ raw_data_assets = load_assets_from_package_module(
     key_prefix=["raw_data", "ducky"],
 )
 
-
-
-dbt_assets = load_assets_from_dbt_project(
-    DBT_PROJECT_DIR,
-    DBT_PROFILES_DIR,
-    # prefix the output assets based on the database they live in plus the name of the schema
+# dbt_assets = load_assets_from_dbt_project(
+#     project_dir=DBT_PROJECT_DIR,
+#     profiles_dir=DBT_PROFILES_DIR,
+#     # prefix the output assets based on the database they live in plus the name of the schema
+#     key_prefix=["duckdb", "ducky"],
+#     # prefix the source assets based on just the database
+#     # (dagster populates the source schema information automatically)
+#     source_key_prefix=["ducky"],
+#     )
+dbt_assets=load_assets_from_dbt_manifest(
+    Path(MANIFEST_PATH), 
     key_prefix=["duckdb", "ducky"],
     # prefix the source assets based on just the database
     # (dagster populates the source schema information automatically)
