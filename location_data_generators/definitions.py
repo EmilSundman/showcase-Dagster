825c2b1ce86b4473f7e6cb076aab66b6bdc5e234
EmilSundman <sundman.emil@gmail.com>
2023-07-27 23:49:51 +0200

    Setup of dbt stuff

--- location_data_generators/definitions.py
+++ location_data_generators/definitions.py
@@ -6,7 +6,6 @@ import os
 from dagster import (
     Definitions,
     load_assets_from_package_module,
-    file_relative_path, 
     OpExecutionContext
 )
 
@@ -16,37 +15,24 @@ from dagster_dbt import dbt_cli_resource, load_assets_from_dbt_project, load_ass
 
 from location_data_generators.assets import raw_data
 
-
-MANIFEST_PATH = file_relative_path(__file__, "../../dbt_transformations/target/manifest.json")
-DBT_PROJECT_DIR = file_relative_path(__file__, "../../dbt_transformations")
-DBT_PROFILES_DIR = file_relative_path(__file__, "../../dbt_transformations/config")
-
-MANIFEST_PATH = "./dbt_transformations/target/manifest.json"
-DBT_PROJECT_DIR = "./dbt_transformations"
-DBT_PROFILES_DIR = "./dbt_transformations/config"
+MANIFEST_PATH = "/opt/dagster/app/dbt_transformations/target/manifest.json"
+DBT_PROJECT_DIR = "/opt/dagster/app/dbt_transformations/."
+DBT_PROFILES_DIR = "/opt/dagster/app/dbt_transformations/config/."
 
 raw_data_assets = load_assets_from_package_module(
     raw_data,
-    group_name="data_loaders",
+    group_name="CSV_loaders",
     # all of these assets live in the duckdb database, under the schema raw_data
-    key_prefix=["raw_data", "ducky"],
+    key_prefix=["raw_data", "duckster"],
 )
 
-# dbt_assets = load_assets_from_dbt_project(
-#     project_dir=DBT_PROJECT_DIR,
-#     profiles_dir=DBT_PROFILES_DIR,
-#     # prefix the output assets based on the database they live in plus the name of the schema
-#     key_prefix=["duckdb", "ducky"],
-#     # prefix the source assets based on just the database
-#     # (dagster populates the source schema information automatically)
-#     source_key_prefix=["ducky"],
-#     )
+
 dbt_assets=load_assets_from_dbt_manifest(
     Path(MANIFEST_PATH), 
-    key_prefix=["duckdb", "ducky"],
+    key_prefix=["duckdb", "duckster"],
     # prefix the source assets based on just the database
     # (dagster populates the source schema information automatically)
-    source_key_prefix=["ducky"],
+    source_key_prefix=["duckster"],
     )
 
 resources = {
